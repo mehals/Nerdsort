b4e2d564d83b3aa500bea26d700ed1b86710d34b
mehals <mehal.shah@gmail.com>
2013-12-22 21:49:39 -0800

    Vote

---
+++ Nerdsort/src/main/java/com/mehal/nerdsort/types/Vote.java
@@ -0,0 +1,18 @@
+package com.mehal.nerdsort.types;
+
+public class Vote {
+	private String voteString;
+
+	public String getVoteString() {
+		return voteString;
+	}
+
+	public void setVoteString(String voteString) {
+		this.voteString = voteString;
+	}
+	
+	public String toString() {
+		return voteString;
+	}
+	
+}
